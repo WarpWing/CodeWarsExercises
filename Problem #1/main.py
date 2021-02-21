abe2a0de3bbd847b4dc5e1569ff3a731c1ecd45a
WarpWing <professornitro@gmail.com>
2021-02-21 15:30:27 -0500

    Problem #1 Commit

---
+++ Problem #1/main.py
@@ -0,0 +1,17 @@
+# Main List to input 
+l = [1,2,'a','b'] #Sample list
+
+def filter_list(l): 
+    for i in l: # Iterates over said list
+        if type(i) is int: #Checks for object type.
+            continue
+        else:
+            l.remove(i) # Remove said string.
+            filter_list(l) # Repeats function again.
+
+    return l # Returns output
+            
+
+filter_list(l) # Executes functions 
+
+

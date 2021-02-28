aa6f0d0e14459d2ce197f3133a5d620f36262ec2
Ty Chermsirivatana <professornitro@gmail.com>
2021-02-27 21:26:15 -0500

    Update main.py

--- Problem #3/main.py
+++ Problem #3/main.py
@@ -2,22 +2,16 @@ year = input("Enter your input: ")
 
 def century(year):
     year = str(year)
-    if year.endswith("00") and len(year) > 5:
-        return int(year[0])
+    if year.endswith("00"):
+        return int(year[:2])
     elif year.endswith("00") and len(year) <= 3:
         return int(year[0])
-    elif len(year) == 1 or len(year) == 2 and int(year) >= 1:
+    elif len(year) == 2:
             return 1
     elif len(year) == 3:
         return int(year[0]) + 1 
     elif len(year) == 4:
         return int(year[:2]) + 1 
-    elif len(year) < 5: 
-        return 'Please input a integer with 4 digits or less.' 
-    elif int(year) <= -1:
-        return 'Please input a integer with positive values.'
-    else: 
-        return 'Please input a valid value to use this program correctly'
     
 
 print(century(year))
